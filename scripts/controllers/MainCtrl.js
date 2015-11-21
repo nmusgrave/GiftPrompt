f190fc6ac71f909af518b6b696eb2588ce13895e
nmusgrave <naomi.g.musgrave@gmail.com>
2015-11-21 08:44:51 -0800

    rename controlle

--- scripts/controllers/MainCtrl.js
+++ scripts/controllers/MainCtrl.js
@@ -1,13 +1,9 @@
 // create the controller and inject Angular's $scope
-app.controller('SignInCtrl', function($scope) {
+app.controller('MainCtrl', function($scope) {
   // create a message to display in our view
   $scope.header = 'Secret Santa';
   $scope.create = 'Create Group'
   
-  $scope.dir = 'images';
-  $scope.horizontal = [ 'gorgeB&W.jpg','IMG4029.JPG' ];
-  $scope.vertical = [ 'road.jpg', 'trees.jpg', 'IMG3722.JPG', 'IMG4027.JPG' ];
-
   var init = function() {
     console.log("hello")
   };
